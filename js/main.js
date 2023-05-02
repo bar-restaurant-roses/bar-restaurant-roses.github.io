f29ea9318e77a3b7f892f2b44a7d640bf1acd3ba
Marc Morente <mmorente@inthinking.co.uk>
2023-05-02 23:37:56 +0200

    add scroll-up float button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 const divTapas = document.querySelector('#tapas');
+const divBocadillos = document.querySelector('#bocadillos');
+const divTorradas = document.querySelector('#torradas');
+
 tapas.forEach(tapa => {
-    divTapas.innerHTML += `
+    divTapas.innerHTML += card(tapa);
+});
+
+document.querySelector("#scroll-up").addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+function card(food) {
+    return `
         <div class="col mb-4">
             <div class="card shadow-sm">
-                <img src="${tapa.image}" class="card-img-top" alt="...">
+                <img src="${food.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${tapa.name}</h5>
+                    <h5 class="card-title">${food.name}</h5>
                     <div class="d-flex justify-content-between align-items-center">
-                        <span class="badge rounded-pill text-bg-danger fs-7">${tapa.price}</span>
+                        <span class="badge rounded-pill text-bg-danger fs-7">${food.price}</span>
                     </div>
                 </div>
             </div>
         </div>
     `;
-});
+}
